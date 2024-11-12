809f6e5bbacc9ea7e45881d11a7a6ec47fa73ac8
Almaz <shakurov-40@mail.ru>
2024-11-12 22:18:49 +0400

    test

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"net/http"
-	"net/http/httptest"
 	"strconv"
 	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 var cafeList = map[string][]string{
@@ -47,25 +42,4 @@ func mainHandle(w http.ResponseWriter, req *http.Request) {
 
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(answer))
-}
-
-func TestMainHandlerWhenCountMoreThanTotal(t *testing.T) {
-	totalCount := 4
-	req := httptest.NewRequest("GET", "/cafe?count=10&city=moscow", nil)
-
-	responseRecorder := httptest.NewRecorder()
-	handler := http.HandlerFunc(mainHandle)
-	handler.ServeHTTP(responseRecorder, req) // здесь нужно создать запрос к сервису
-	body := responseRecorder.Body.String()
-	status := responseRecorder.Code
-
-	require.Equal(t, status, http.StatusOK)
-	require.NotEmpty(t, responseRecorder.Body)
-
-	assert.Equal(t, status, http.StatusBadRequest)
-	assert.Equal(t, body, "wrong city value")
-
-	list := strings.Split(body, ",")
-	assert.Len(t, list, totalCount)
-	// здесь нужно добавить необходимые проверки
 }
